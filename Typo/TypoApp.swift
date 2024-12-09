44811b85b67b480affe113fc9d1187829d42a398
ercode1337 <eric.melin@outlook.com>
2024-12-09 19:56:58 +0200

    Initial Commit

---
+++ Typo/TypoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypoApp.swift
+//  Typo
+//
+//  Created by Eric Melin on 9.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct TypoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TypoTests/TypoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TypoTests.swift
+//  TypoTests
+//
+//  Created by Eric Melin on 9.12.2024.
+//
+
+import Testing
+@testable import Typo
+
+struct TypoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
